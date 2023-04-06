a0df51982b5507f3a2628b5cbe00c9e2c7873cab
Jack Kamaryt <jkamaryt46@gmail.com>
2023-04-06 09:55:10 -0500

    Initial Commit

---
+++ QAJF Barrington School Student Tool/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QAJF Barrington School Student Tool
+//
+//  Created by Jack Kamaryt on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QAJF Barrington School Student Tool/QAJF_Barrington_School_Student_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  QAJF_Barrington_School_Student_ToolApp.swift
+//  QAJF Barrington School Student Tool
+//
+//  Created by Jack Kamaryt on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct QAJF_Barrington_School_Student_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
